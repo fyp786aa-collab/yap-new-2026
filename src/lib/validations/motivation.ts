4bb89be53e7c90f5137e14af76a3a03646b71e1f
ibs <iqbalshahnadiri786@gmail.com>
2026-03-15 03:07:05 +0500

    correct word limit

--- src/lib/validations/motivation.ts
+++ src/lib/validations/motivation.ts
@@ -9,8 +9,8 @@ export const motivationSchema = z.object({
   essay_response: z
     .string()
     .min(1, "Please write your motivation essay")
-    .refine((val) => wordCount(val) <= 150, {
-      message: "Essay must not exceed 150 words",
+    .refine((val) => wordCount(val) <= 250, {
+      message: "Essay must not exceed 250 words",
     }),
   scenario_response: z
     .string()
